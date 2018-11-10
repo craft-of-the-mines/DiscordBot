8fb04d473e864ec9fef3f2046fc0ba68102a3920
ライアン <ryan.wachowski@duethealth.com>
2018-11-10 02:20:11 -0500

    ehehehe time to roast some script kiddies

--- Sources/DuetBot/main.swift
+++ Sources/DuetBot/main.swift
@@ -1,15 +1,25 @@
 import Sword
 
 let bot = Sword(token: "token")
+var insultUsernames = [String]()
 
 bot.editStatus(to: "online", playing: "with Sword!")
 
 bot.on(.messageCreate) { data in
-  let msg = data as! Message
+    guard let message = data as? Message else { return }
+    if let acquiredTarget = message.author?.username, insultUsernames.contains(acquiredTarget) {
+        return message.reply(with: "Hey, screw you \(acquiredTarget)!")
+    }
+    
+    switch message.content {
+    case "!ping": message.reply(with: "Pong!")
+    case let toInsult where toInsult.hasPrefix("!insult "):
+        let username = String(toInsult.dropFirst(8))
+        insultUsernames.append(username)
+        message.reply(with: "Okay, whenever \(username) tries to message me they'll regret it!")
+    default: break
+    }
 
-  if msg.content == "!ping" {
-    msg.reply(with: "Pong!")
-  }
 }
 
 bot.connect()
