4771e5591e72f9df43ca9482c471902d29085302
ライアン <ryan.wachowski@duethealth.com>
2018-11-10 19:48:12 -0500

    Cleaner exit path when missing JSON

--- Sources/DuetBot/main.swift
+++ Sources/DuetBot/main.swift
@@ -1,23 +1,25 @@
 import Foundation
 import Sword
 
+let missingConfig: () -> Never = {
+    print("""
+    You're missing a valid config.json file in the current directory.
+    It should contain JSON which can be decoded into a Config instance.
+    """)
+    #if DEBUG
+    print("""
+    When running in debug: the target contains a copy build phase which
+    copies a config.json file at $(SRCROOT)/Sources/DuetBot. Make sure
+    that the Products directory for this target (somewhere in your
+    DerivedData) contains this file.
+    """)
+    #endif
+    exit(1)
+}
+
 guard let config = FileManager.default
     .contents(atPath: FileManager.default.currentDirectoryPath.appending("/config.json"))
-    .flatMap({ try? JSONDecoder().decode(Config.self, from: $0) }) else {
-        print("""
-        You're missing a valid config.json file in the current directory.
-        It should contain JSON which can be decoded into a Config instance.
-        """)
-        #if DEBUG
-        print("""
-        When running in debug: the target contains a copy build phase which
-        copies a config.json file at $(SRCROOT)/Sources/DuetBot. Make sure
-        that the Products directory for this target (somewhere in your
-        DerivedData) contains this file.
-        """)
-        #endif
-        exit(1)
-}
+    .flatMap({ try? JSONDecoder().decode(Config.self, from: $0) }) else { missingConfig() }
 
 Bot(token: config.token, routines:
     PingRoutine(),
