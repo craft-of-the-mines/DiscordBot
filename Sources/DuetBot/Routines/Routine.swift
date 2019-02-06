f0f3be267dd84daa733b8cd74f2189f0aff91afc
Ryan <ryan@raian.local>
2019-02-05 19:01:02 -0500

    Comment about how to fix up Routine to work on any incoming message.

--- Sources/DuetBot/Routines/Routine.swift
+++ Sources/DuetBot/Routines/Routine.swift
@@ -16,7 +16,18 @@ func ~=(_ routine: Routine, _ command: Command) -> Bool {
 // example, `PingPongRoutine` has to be two separate routines instead of a single routine with
 // different subroutines based on whether ping or pong was said.
 protocol Routine: AnyObject {
+    // NOTE: Here's where the major defect is. Right now, this only allows a routine
+    // to match against explicitly-defined patterns. You kind of want an ADT/discriminated
+    // union here so that you can still represent explicit keywords but also the catch-all
+    // case. With this functionality, your minecraft message function would match against
+    // any incoming "command". See the note below about what happens after the messaging
+    // routine matches.
     static var patterns: [String] { get }
+    // NOTE: This should probably just take the entire command as an argument.
+    // Commands should store the raw text too. This way, your routines can be
+    // more flexible about the incoming data and optionally use the convenience
+    // properties (i.e. Command.arguments) to make the code cleaner.
+    // e.g. observe(command: Command)
     func observe(arguments: [String]) -> Reply
 }
 
